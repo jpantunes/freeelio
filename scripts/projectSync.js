a1300ad6865542eb32ffc34d222f0f75bf4b5557
Skirmantas Januskas <skirmantas.januskas@gmail.com>
2018-03-08 19:08:53 +0100

    Solshare API class

--- scripts/projectSync.js
+++
@@ -1,4 +0,0 @@
-let request = require('request');
-let SolshareApi = require('./solshareApi')(request);
-
-// call API and write data to blockchain
---
+++ src/utils/solshareApi.js
@@ -0,0 +1,40 @@
+module.exports = function () {
+    class SolshareApi {
+        static getCredentials () {
+            return {
+                username: "008801111111987",
+                password: "free2018lio",
+            };
+        };
+
+        static getDonationBoxesUrl () {
+            let credentials = this.getCredentials();
+            let url = "https://";
+            url += credentials.username + ":" + credentials.password + '@';
+            url += 'api.me-solshare.com/api/v1/solbox/donation_boxes';
+
+            return url;
+        }
+
+        static getDonationBoxes(callback) {
+            let request = require('request');
+            request({url: SolshareApi.getDonationBoxesUrl(), json:true}, function (error, response, body) {
+                let solboxes = Object.keys(body[1].solboxes).map(function (key) {
+                    console.log(Object.values(body[1].solboxes[key]));
+                    return Object.values(body[1].solboxes[key]);
+                });
+
+                /* Returning array with values:
+                solbox_id,
+                grid_id,
+                amount_of_energy_bought_in_last_30_days,
+                total_bought_in_last_30_days,
+                recharge_amount_to_be_allocated
+                 */
+                return callback(solboxes);
+            });
+        }
+    }
+
+    return SolshareApi;
+};
